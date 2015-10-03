b87bf7d1448d51f2e366264210ec303208f8f66e
semihxx <semih.tryk@gmail.com>
2015-10-03 05:55:03 +0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,10 +1,4 @@
-    /* 
-    content script(main.js) içinde sayfa her sayfanın yüklenişini dinleyemediğim için böyle yaptım. 
-    Alternatif çözümü olabilir. fazla bilgi sahibi değilim.
-	
-	Tahminim content scriptler sayfanın içerisinde doğrudan eklendiği için tarayıcıya ait tab eventleri 
-	dinlemek mümkün olmuyor.
-    */
+
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	//her sayfa açıldığında tetiklenecek
