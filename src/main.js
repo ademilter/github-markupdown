e302b56e5bddffef6c2b6a238dba72f3cab1c55c
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2015-10-04 14:34:51 +0300

    Make some improvements.

--- src/main.js
+++ src/main.js
@@ -20,9 +20,9 @@ function MarkupDown(textarea) {
 
   this.$el.on('action', function (event, actionName) {
     var area = _this.textarea;
-    var selectedText = area.value.substr(area.selectionStart, area.selectionEnd - area.selectionStart);
-    var replacementText = _this[actionName](selectedText);
-    _this.applyReplacement(replacementText);
+    var selectedText = area.value.substr(area.selectionStart, area.selectionEnd - area.selectionStart).trim();
+    var replacementText = _this[actionName](selectedText, area.value);
+    _this.applyReplacement(replacementText, !!selectedText);
   });
 }
 
@@ -56,7 +56,10 @@ MarkupDown.prototype.bindEvents = function () {
 };
 
 MarkupDown.prototype.link = function (selectedText) {
-  return "[" + selectedText + "](" + prompt("Please enter your link", "") + ")";
+  var link = prompt("Please enter your link", "");
+  var text = selectedText || link;
+
+  return "[" + text + "](" + link + ")";
 };
 
 MarkupDown.prototype.bold = function (selectedText) {
@@ -72,27 +75,52 @@ MarkupDown.prototype.insert = function (selectedText) {
 };
 
 MarkupDown.prototype.code = function (selectedText) {
+  if (/\n/.test(selectedText)) { // if code is multilined, make it bigger.
+    return "\n```" + prompt("What language is this code?") + "\n" + selectedText + "\n```\n";
+  }
   return this.toggleWrap('`', selectedText);
 };
 
 MarkupDown.prototype.quote = function (selectedText) {
-  return "\n> " + selectedText + "\n";
+  return this.multilineReplacer(selectedText, function (listItem) {
+    return '> ' + listItem;
+  });
 };
 
 MarkupDown.prototype.ul = function (selectedText) {
-  return "\n- " + selectedText;
+  return this.multilineReplacer(selectedText, function (listItem) {
+    return '- ' + listItem;
+  });
 };
 
-MarkupDown.prototype.ol = function (selectedText) {
-  return "\n1. " + selectedText;
+MarkupDown.prototype.ol = function (selectedText, allText) {
+  var lastItemIndex = 0;
+  var beforePart;
+  if (selectedText) {
+    beforePart = allText.substr(0, allText.indexOf(selectedText));
+  } else {
+    beforePart = allText;
+  }
+  var findItems = beforePart.match(/^(\d+)\./gm);
+  if (findItems) {
+    lastItemIndex = +findItems.pop().replace(/\D+/, '');
+  }
+
+  return this.multilineReplacer(selectedText, function (listItem, index) {
+    return (lastItemIndex + index + 1) + '. ' + listItem;
+  });
 };
 
 MarkupDown.prototype.check = function (selectedText) {
-  return "\n- [ ] " + selectedText;
+  return this.multilineReplacer(selectedText, function (listItem) {
+    return '- [] ' + listItem;
+  });
 };
 
 MarkupDown.prototype.checked = function (selectedText) {
-  return "\n- [x] " + selectedText;
+  return this.multilineReplacer(selectedText, function (listItem) {
+    return '- [x] ' + listItem;
+  });
 };
 
 // Helper Functions
@@ -108,7 +136,11 @@ MarkupDown.prototype.toggleWrap = function (wrapper, text) {
   }
 };
 
-MarkupDown.prototype.applyReplacement = function (replacementText) {
+MarkupDown.prototype.multilineReplacer = function (replacementText, mapper) {
+  return "\n" + replacementText.split(/\n/gm).map(mapper).join("\n");
+};
+
+MarkupDown.prototype.applyReplacement = function (replacementText, canBeSelected) {
   var area = this.textarea;
   var beforePart = area.value.substr(0, area.selectionStart);
   var afterPart = area.value.substr(area.selectionEnd);
@@ -116,9 +148,19 @@ MarkupDown.prototype.applyReplacement = function (replacementText) {
   var rangeStart = area.selectionStart;
   var rangeEnd = rangeStart + replacementText.length;
 
+  if (!beforePart.trim()) {
+    // If there's no before part, we don't need spaces before replacement.
+    replacementText = replacementText.replace(/^\n/g, '');
+  }
+
   area.value = beforePart + replacementText + afterPart;
   area.focus();
-  area.setSelectionRange(rangeStart, rangeEnd);
+  if (canBeSelected === true) {
+    area.setSelectionRange(rangeStart, rangeEnd);
+  } else {
+    // Move cursor to the end of selection.
+    area.setSelectionRange(rangeEnd, rangeEnd);
+  }
 };
 
 function init() {
